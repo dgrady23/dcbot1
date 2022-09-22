fad4096a82521e1f04e81685de736f560f68e791
dgrady23 <packsotn5@gmail.com>
2022-09-22 12:33:40 -0400

    command delete

--- deploy-commands.js
+++ deploy-commands.js
@@ -11,6 +11,11 @@ const commands = [
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+	.then(() => console.log('Successfully deleted all guild commands.'))
+	.catch(console.error);
+
+// for global commands
+rest.put(Routes.applicationCommands(clientId), { body: [] })
+	.then(() => console.log('Successfully deleted all application commands.'))
 	.catch(console.error);
